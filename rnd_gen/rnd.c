ee562c477d99efb5a19254dbc741da70e7dc316f
Eric LI <erli@student.42.fr>
2019-01-14 17:25:04 +0100

    Ajout d'un usage a rnd

--- rnd_gen/rnd.c
+++ rnd_gen/rnd.c
@@ -7,7 +7,7 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 16:55:24 by erli              #+#    #+#             */
-/*   Updated: 2019/01/14 17:21:17 by erli             ###   ########.fr       */
+/*   Updated: 2019/01/14 17:24:31 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ static	int		rnd(int min, int max, int n)
 int				main(int argc, char **argv)
 {
 	if (argc < 4)
-		return (0);
+		return (ft_msg_int(2, "./rnd min max num\n", 0));
 	rnd(ft_atoi(argv[1]), ft_atoi(argv[2]), ft_atoi(argv[3]));
 	return (0);
 }
